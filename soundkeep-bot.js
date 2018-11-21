a0b3d641d13dea14ffbf894865d4e8bc0cc625a6
Jesse <json469@aucklanduni.ac.nz>
2018-11-21 13:39:27 +1300

    Init

---
+++ soundkeep-bot.js
@@ -0,0 +1,20 @@
+var Discord = require('discord.io');
+var config = require('./auth.json')
+
+var bot = new Discord.Client({
+    autorun: true,
+    token: config.token
+});
+
+bot.on('ready', function(event) {
+    console.log('Logged in as %s - %s\n', bot.username, bot.id);
+});
+
+bot.on('message', function(user, userID, channelID, message, event) {
+    if (message === "ping") {
+        bot.sendMessage({
+            to: channelID,
+            message: "pong"
+        });
+    }
+});
